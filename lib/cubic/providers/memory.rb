9372751cec426320b3472a668667feb79af49a68
Renato Mascarenhas <renato@roomorama.com>
2016-02-17 17:30:33 +0800

    Include memory provider for interface testing.

---
+++ lib/cubic/providers/memory.rb
@@ -0,0 +1,36 @@
+module Cubic
+  module Providers
+
+    class Memory
+      attr_reader :storage
+
+      def initialize
+        @storage = {}
+      end
+
+      def inc(label, by: 1)
+        storage[label] ||= 0
+        storage[label] += by
+      end
+
+      def val(label, value)
+        storage[label] ||= []
+        storage[label] << value
+      end
+
+      def time(label)
+        start = Time.now
+        yield
+      ensure
+        duration_in_ms = ((Time.now - start) * 1000).round
+        val(label, duration_in_ms)
+      end
+
+      def query(label)
+        storage[label]
+      end
+
+    end
+
+  end
+end
