89209cac0c127537c8e6b17ab86a7b4dd8f92e84
duykhoa <duykhoa12t@gmail.com>
2016-06-28 18:06:25 +0800

    Test for submit_librato method

--- test/cubic/workers/test_librato.rb
+++ test/cubic/workers/test_librato.rb
@@ -11,14 +11,44 @@ class Redis
   end
 end
 
+module Librato::Metrics
+  class << self
+    def reset!
+      @_calls = []
+    end
+
+    def _calls
+      @_calls ||= []
+    end
+
+    def submit(options)
+      _calls << options
+    end
+  end
+end
+
 class TestBaseWorker < Minitest::Test
   def setup
-    @worker = Cubic::Workers::Librato.new
+    config = {
+      email: "librato@example.org",
+      api_key: "12345",
+      source: "test",
+      namespace: "dev"
+    }
+
+    @worker = Cubic::Workers::Librato.new(config)
   end
 
   def test_load_redis_metrics
     result = @worker.load_redis_metrics
     assert_includes result.names, "foo"
     assert_includes result.names, "bar"
+  end
+
+  def test_submit_librato
+    metrics = Cubic::Workers::Librato::Metric.new(["foo"], [1])
+    @worker.submit_librato(metrics)
+    result = Librato::Metrics._calls
+    assert_instance_of Array, result
   end
 end
